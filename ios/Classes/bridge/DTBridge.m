9b06ab8e254dee0c3f2d7468eff9a4090d8bb819
Lilin <lilinll77@gmail.com>
2023-09-20 18:41:34 +0800

    flutter_sdk, 桥接层实现

---
+++ ios/Classes/bridge/DTBridge.m
@@ -0,0 +1,18 @@
+//
+//  DTBridge.m
+//  datatower_ai_core_flutter
+//
+//  Created by Lilin on 2023/9/20.
+//
+
+#import "DTBridge.h"
+#import <DataTowerAICore/DT.h>
+
+@implementation DTBridge
+
+- (void)initSDKAppId:(nonnull NSString *)appId url:(nonnull NSString *)url channel:(nonnull NSString *)channel isDebug:(nonnull NSNumber *)isDebug logLevel:(nonnull NSNumber *)logLevel commonProperties:(nonnull NSDictionary<NSString *,id> *)commonProperties error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
+    
+    [DT initSDK:appId serverUrl:url channel:[channel intValue] isDebug:[isDebug boolValue] logLevel:[logLevel intValue]];
+}
+
+@end
