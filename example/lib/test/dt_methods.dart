b200bbfd857cac2200b3be93670ca83a09b32598
linkailong <linkailong@lovinjoy.com>
2023-10-19 15:01:06 +0800

    Use publish.sh to publish to pub.dev with checks

--- example/lib/test/dt_methods.dart
+++ example/lib/test/dt_methods.dart
@@ -5,36 +5,18 @@
 // **************************************************************************
 
 import 'package:datatower_ai_core_flutter/api/dt.dart';
-import 'package:datatower_ai_core_flutter/api/dt_ad.dart';
 import 'package:datatower_ai_core_flutter/api/dt_analytics.dart';
+import 'package:datatower_ai_core_flutter/api/dt_ad.dart';
 import 'package:datatower_ai_core_flutter/api/dt_analytics_util.dart';
 
 const List<DtApiMethodHolder> dtApiMethodHolders = [
   dtApiMethods4DT,
-  dtApiMethods4DTAdReport,
   dtApiMethods4DTAnalytics,
+  dtApiMethods4DTAdReport,
   dtApiMethods4DTAnalyticsUtil
 ];
 
 const DtApiMethodHolder dtApiMethods4DT = DtApiMethodHolder("DT", ["initSDK"]);
-
-const DtApiMethodHolder dtApiMethods4DTAdReport =
-    DtApiMethodHolder("DTAdReport", [
-  "reportLoadBegin",
-  "reportLoadEnd",
-  "reportToShow",
-  "reportShow",
-  "reportShowFailed",
-  "reportClose",
-  "reportClick",
-  "reportRewarded",
-  "reportConversionByClick",
-  "reportConversionByLeftApp",
-  "reportConversionByRewarded",
-  "reportPaid",
-  "reportPaidWithCountry",
-  "reportLeftApp"
-]);
 
 const DtApiMethodHolder dtApiMethods4DTAnalytics =
     DtApiMethodHolder("DTAnalytics", [
@@ -53,6 +35,24 @@ const DtApiMethodHolder dtApiMethods4DTAnalytics =
   "setKochavaId",
   "setAdjustId",
   "enableThirdPartySharing"
+]);
+
+const DtApiMethodHolder dtApiMethods4DTAdReport =
+    DtApiMethodHolder("DTAdReport", [
+  "reportLoadBegin",
+  "reportLoadEnd",
+  "reportToShow",
+  "reportShow",
+  "reportShowFailed",
+  "reportClose",
+  "reportClick",
+  "reportRewarded",
+  "reportConversionByClick",
+  "reportConversionByLeftApp",
+  "reportConversionByRewarded",
+  "reportPaid",
+  "reportPaidWithCountry",
+  "reportLeftApp"
 ]);
 
 const DtApiMethodHolder dtApiMethods4DTAnalyticsUtil = DtApiMethodHolder(
@@ -91,9 +91,162 @@ final Map<String, DtApiMethod> dtApiMethods = {
       name: "DT",
       orderedParamNames: ["appId", "url", "channel", "isDebug", "logLevel"],
       orderedParam: ["String", "String", "String", "bool", "int"],
-      namedParam: {"commonProperties": "Map<String, Object?>"},
+      namedParam: {},
       run: (ordered, named) {
-        DT.initSDK(ordered[0], ordered[1], ordered[2], ordered[3], ordered[4]);
+        DT.initSDK(
+          ordered[0],
+          ordered[1],
+          ordered[2],
+          ordered[3],
+          ordered[4],
+        );
+      }),
+  "DTAnalytics_trackEvent": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["eventName", "properties"],
+      orderedParam: ["String", "Map<String, Object?>"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.trackEvent(
+          ordered[0],
+          ordered[1],
+        );
+      }),
+  "DTAnalytics_userSet": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["properties"],
+      orderedParam: ["Map<String, Object?>"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.userSet(
+          ordered[0],
+        );
+      }),
+  "DTAnalytics_userSetOnce": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["properties"],
+      orderedParam: ["Map<String, Object?>"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.userSetOnce(
+          ordered[0],
+        );
+      }),
+  "DTAnalytics_userAdd": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["properties"],
+      orderedParam: ["Map<String, Object?>"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.userAdd(
+          ordered[0],
+        );
+      }),
+  "DTAnalytics_userUnset": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["properties"],
+      orderedParam: ["List<String>"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.userUnset(
+          ordered[0],
+        );
+      }),
+  "DTAnalytics_userDelete": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: [],
+      orderedParam: [],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.userDelete();
+      }),
+  "DTAnalytics_userAppend": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["properties"],
+      orderedParam: ["Map<String, Object?>"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.userAppend(
+          ordered[0],
+        );
+      }),
+  "DTAnalytics_userUniqAppend": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["properties"],
+      orderedParam: ["Map<String, Object?>"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.userUniqAppend(
+          ordered[0],
+        );
+      }),
+  "DTAnalytics_getDataTowerId": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: [],
+      orderedParam: [],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.getDataTowerId();
+      }),
+  "DTAnalytics_setAccountId": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["id"],
+      orderedParam: ["String?"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.setAccountId(
+          ordered[0],
+        );
+      }),
+  "DTAnalytics_setFirebaseAppInstanceId": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["id"],
+      orderedParam: ["String?"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.setFirebaseAppInstanceId(
+          ordered[0],
+        );
+      }),
+  "DTAnalytics_setAppsFlyerId": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["id"],
+      orderedParam: ["String?"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.setAppsFlyerId(
+          ordered[0],
+        );
+      }),
+  "DTAnalytics_setKochavaId": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["id"],
+      orderedParam: ["String?"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.setKochavaId(
+          ordered[0],
+        );
+      }),
+  "DTAnalytics_setAdjustId": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["id"],
+      orderedParam: ["String?"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.setAdjustId(
+          ordered[0],
+        );
+      }),
+  "DTAnalytics_enableThirdPartySharing": DtApiMethod(
+      name: "DTAnalytics",
+      orderedParamNames: ["type"],
+      orderedParam: ["int"],
+      namedParam: {},
+      run: (ordered, named) {
+        DTAnalytics.enableThirdPartySharing(
+          ordered[0],
+        );
       }),
   "DTAdReport_reportLoadBegin": DtApiMethod(
       name: "DTAdReport",
@@ -541,153 +694,6 @@ final Map<String, DtApiMethod> dtApiMethods = {
         DTAdReport.reportLeftApp(ordered[0], ordered[1], ordered[2], ordered[3],
             ordered[4], ordered[5], ordered[6],
             properties: named["properties"], entrance: named["entrance"]);
-      }),
-  "DTAnalytics_trackEvent": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["eventName", "properties"],
-      orderedParam: ["String", "Map<String, Object?>"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.trackEvent(
-          ordered[0],
-          ordered[1],
-        );
-      }),
-  "DTAnalytics_userSet": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["properties"],
-      orderedParam: ["Map<String, Object?>"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.userSet(
-          ordered[0],
-        );
-      }),
-  "DTAnalytics_userSetOnce": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["properties"],
-      orderedParam: ["Map<String, Object?>"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.userSetOnce(
-          ordered[0],
-        );
-      }),
-  "DTAnalytics_userAdd": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["properties"],
-      orderedParam: ["Map<String, Object?>"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.userAdd(
-          ordered[0],
-        );
-      }),
-  "DTAnalytics_userUnset": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["properties"],
-      orderedParam: ["List<String>"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.userUnset(
-          ordered[0],
-        );
-      }),
-  "DTAnalytics_userDelete": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: [],
-      orderedParam: [],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.userDelete();
-      }),
-  "DTAnalytics_userAppend": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["properties"],
-      orderedParam: ["Map<String, Object?>"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.userAppend(
-          ordered[0],
-        );
-      }),
-  "DTAnalytics_userUniqAppend": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["properties"],
-      orderedParam: ["Map<String, Object?>"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.userUniqAppend(
-          ordered[0],
-        );
-      }),
-  "DTAnalytics_getDataTowerId": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: [],
-      orderedParam: [],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.getDataTowerId();
-      }),
-  "DTAnalytics_setAccountId": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["id"],
-      orderedParam: ["String?"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.setAccountId(
-          ordered[0],
-        );
-      }),
-  "DTAnalytics_setFirebaseAppInstanceId": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["id"],
-      orderedParam: ["String?"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.setFirebaseAppInstanceId(
-          ordered[0],
-        );
-      }),
-  "DTAnalytics_setAppsFlyerId": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["id"],
-      orderedParam: ["String?"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.setAppsFlyerId(
-          ordered[0],
-        );
-      }),
-  "DTAnalytics_setKochavaId": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["id"],
-      orderedParam: ["String?"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.setKochavaId(
-          ordered[0],
-        );
-      }),
-  "DTAnalytics_setAdjustId": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["id"],
-      orderedParam: ["String?"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.setAdjustId(
-          ordered[0],
-        );
-      }),
-  "DTAnalytics_enableThirdPartySharing": DtApiMethod(
-      name: "DTAnalytics",
-      orderedParamNames: ["type"],
-      orderedParam: ["int"],
-      namedParam: {},
-      run: (ordered, named) {
-        DTAnalytics.enableThirdPartySharing(
-          ordered[0],
-        );
       }),
   "DTAnalyticsUtil_trackTimerStart": DtApiMethod(
       name: "DTAnalyticsUtil",
--- publish.sh
+++ publish.sh
@@ -1,9 +1,24 @@
-echo "Starts to verify DT Flutter..."
+echo "Starts checking and publishing process..."
 
 project_path="$(dirname "$0")"
 echo "Project path: $project_path"
 
+thin_divider="--------------------------------------------------------"
 divider="========================================================"
+echo "$divider"
+
+printf "Run \033[0;34mgenerate_dt_api_methods.sh\033[0m? [Enter \033[1;34mY\033[0m or \033[1;34my\033[0m to run, otherwise to quit]: "
+read -r run_publish
+if [[ $run_publish == "Y"  ]] || [[ $run_publish == "y" ]]; then
+  echo "Running \033[0;34mgenerate_dt_api_methods.sh\033[0m..."
+  sh "$project_path/generate_dt_api_methods.sh"
+else
+  echo "Ignored generate_dt_api_methods"
+fi
+
+total_num_api="$(grep -c -E '"[A-Za-z0-9]*_[A-Za-z0-9]*\": DtApiMethod\(' < "$project_path/example/lib/test/dt_methods.dart")"
+echo "Total Number of APIs in dt_methods.dart: \033[4m$total_num_api\033[0m"
+
 echo "$divider"
 
 # shellcheck disable=SC2002
@@ -33,17 +48,43 @@ ios_sdk_ver=$(cat "$project_path/ios/datatower_ai_core_flutter.podspec" | grep "
 echo "Used version of \033[1mDT Android\033[0m：\033[4m$android_sdk_ver\033[0m"
 echo "Used version of \033[1mDT iOS\033[0m：\033[4m$ios_sdk_ver\033[0m"
 
-echo "$divider"
+echo "$thin_divider"
+
+echo "Getting the latest DT iOS/Android version from \033[1mremote...\033[0m"
+android_sdk_remote_ver=$(curl -s https://s01.oss.sonatype.org/content/repositories/releases/com/lovinjoy/datatowerai-core/maven-metadata.xml | grep "<latest>.*</latest>" | cut -d ">" -f2 | cut -d "<" -f1)
+ios_sdk_remote_ver=$(curl -s https://cocoapods.org/pods/DataTowerAICore | sed -n 's/.*<h1>DataTowerAICore <span>\([0-9a-zA-Z.-]*\)<\/span><\/h1>.*/\1/gp')
+echo " - [\033[1mDT Android\033[0m | Maven Central] \033[4m$android_sdk_remote_ver\033[0m"
+echo " - [\033[1mDT iOS\033[0m | CocoaPods] \033[4m$ios_sdk_remote_ver\033[0m"
 
 if [[ $version_check_failed == true ]]; then
+  echo "$divider"
   echo "\033[0;31mVersion check is failed, pls see details above!\033[0m"
   exit
+else
+  echo "$thin_divider"
+  echo "\033[5;33m ⬆ \033[0;4;33m ⓘ  Please check versions shown above \033[0m"
 fi
 
-echo "\033[0;43m ⓘ  Please check versions shown above\033[0m"
+echo "$divider"
+
+num_find_in_changelog=$(grep -c -E "^#+ ${stripped_sdk_version}" < "$project_path/CHANGELOG.md")
+if [[ $num_find_in_changelog == 0 ]]; then
+  echo "\033[0;31mPublish material check is failed!"
+  echo "  - This version ($stripped_sdk_version) is not listed in \033[1;31mCHANGELOG.md\033[0;31m.\033[0m"
+  exit
+else
+  changelog_lower_bound=$(cat "$project_path/CHANGELOG.md" | grep -FnE -m2 "# .*" | tail -n1 | cut -d ":" -f 1)
+  echo "CHANGELOG"
+  sed -n -e "1,$((changelog_lower_bound-1))p" < ./CHANGELOG.md | sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba'
+fi
+
+echo "$divider"
+
 printf "Run \033[0;34mflutter pub publish\033[0m? [Enter \033[1;34mY\033[0m or \033[1;34my\033[0m to run, otherwise to quit]: "
-read run_publish
+read -r run_publish
 if [[ $run_publish == "Y"  ]] || [[ $run_publish == "y" ]]; then
-  echo "Running \033[0;34mflutter pub upgrade\033[0m..."
+  echo "Running \033[0;34mflutter pub publish\033[0m..."
   flutter pub publish
+else
+  echo "Ignored flutter pub publish"
 fi
