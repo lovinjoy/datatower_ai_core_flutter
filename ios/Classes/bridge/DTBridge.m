e356fbf07d9a99541e0a81b2a07022f87f64ba3e
Lilin <lilinll77@gmail.com>
2023-10-25 12:15:46 +0800

    日志级别各端统一

--- ios/Classes/bridge/DTBridge.m
+++ ios/Classes/bridge/DTBridge.m
@@ -10,9 +10,21 @@
 
 @implementation DTBridge
 
-- (void)initSDKAppId:(nonnull NSString *)appId url:(nonnull NSString *)url channel:(nonnull NSString *)channel isDebug:(nonnull NSNumber *)isDebug logLevel:(nonnull NSNumber *)logLevel commonProperties:(nonnull NSDictionary<NSString *,id> *)commonProperties error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
+- (void)initSDKAppId:(nonnull NSString *)appId url:(nonnull NSString *)url channel:(nonnull NSString *)channel isDebug:(nonnull NSNumber *)isDebug logLevel:(DTDTLogLevel)logLevel commonProperties:(nonnull NSDictionary<NSString *,id> *)commonProperties error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
     
-    [DT initSDK:appId serverUrl:url channel:DTChannelAppStore isDebug:[isDebug boolValue] logLevel:[logLevel intValue] commonProperties:commonProperties];
+    DTLoggingLevel iOSLogLevel = [self convertFlutterLogLevel:logLevel];
+    [DT initSDK:appId serverUrl:url channel:DTChannelAppStore isDebug:[isDebug boolValue] logLevel:iOSLogLevel commonProperties:commonProperties];
+}
+
+- (DTLoggingLevel)convertFlutterLogLevel:(DTDTLogLevel)fltLogLevel {
+    DTLoggingLevel ret = DTLoggingLevelError;
+    if ( fltLogLevel <= DTDTLogLevelDEBUG ) {
+        ret = DTLoggingLevelDebug;
+    } else if ( fltLogLevel <= DTDTLogLevelINFO ) {
+        ret = DTLoggingLevelInfo;
+    } 
+    return ret;
 }
 
 @end
+
